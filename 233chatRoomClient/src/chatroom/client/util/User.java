5df660b88c9a257295d3a5aef0ab765d9d8f541e
giglf <lin.giglf@gmail.com>
2016-10-08 00:31:12 +0800

    first initiate the project and add two useful classes

---
+++ 233chatRoomClient/src/chatroom/client/util/User.java
@@ -0,0 +1,33 @@
+package chatroom.client.util;
+/**
+ * This is a User class for a normal user.<br/>
+ * You should pay attention to the sex information.<br/>
+ * true->male, false->female
+ */
+public class User {
+	
+	private String username;
+	private String password;
+	private boolean sex; //true for male, and false for female.
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isSex() {
+		return sex;
+	}
+	public void setSex(boolean sex) {
+		this.sex = sex;
+	}
+	
+}
