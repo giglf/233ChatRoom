3243f6325700ff6044dc8fe502872c2e829234d7
giglf <lin.giglf@gmail.com>
2016-11-26 17:10:31 +0800

    chatRoom Server is ok (without file transfer)

--- 233chatRoomServer/src/chatroom/server/ChatServer.java
+++ 233chatRoomServer/src/chatroom/server/ChatServer.java
@@ -1,8 +1,11 @@
 package chatroom.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.util.ArrayList;
 import java.util.List;
 
+import chatroom.client.util.Packet;
 import chatroom.server.util.Client;
 
 
@@ -13,10 +16,55 @@ import chatroom.server.util.Client;
 public class ChatServer {
 	
 	private static final int PORT = 23399;
-	
+	private ServerSocket serverSocket;
+
 	private List<Client> clientList = new ArrayList<Client>();
-	//listening->accept()->Packet.Type->
-	//switch 1.RegisterHandle 2.LoginHandler 3.sendMessage 4.sendFile (3 and 4 implement in TransferData.java)
 	
+	public static void main(String[] args) {
+		ChatServer server = new ChatServer();
+		server.startListening(server);
+	}
+	
+	public void startListening(ChatServer chatServer){
+		Thread listen = new Thread(){
+			public void run() {
+				try {
+					serverSocket = new ServerSocket(PORT);
+					while(true){
+						System.out.println("Listening on port " + PORT + " ...");
+						Client newClient = new Client(chatServer, serverSocket);
+						System.out.println("Connected: " + newClient.getSocket().getInetAddress().getHostName());
+						clientList.add(newClient);
+						newClient.listenToConnection();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		listen.start();
+	}
+	
+	//向所有用户广播消息
+	public void broadcast(Packet packet){
+		for(Client client : clientList){
+			try {
+				client.getOutputStream().writeObject(packet);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	//发送用户列表
+	public void sendClientLists(){
+		for(Client client : clientList){
+			broadcast(Packet.sendEnter(client.getConnectUser()));
+		}
+	}
+	
+	public void dropClient(Client client){
+		clientList.remove(client);
+	}
 	
 }
--- 233chatRoomServer/src/chatroom/server/util/Client.java
+++ 233chatRoomServer/src/chatroom/server/util/Client.java
@@ -6,22 +6,85 @@ import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import chatroom.client.util.Packet;
 import chatroom.client.util.User;
+import chatroom.server.ChatServer;
 
-public class Client extends User{
+public class Client{
 	
-	
+	private ChatServer server;
 	private Socket clientSocket;
 	private ObjectOutputStream outputStream;
 	private ObjectInputStream inputStream;
 	
-	public Client(User user, Socket gSocket) throws IOException {
-		super(user);
-		//clientSocket = serverSocket.accept();
-		clientSocket = gSocket;
+	private User connectUser;
+	
+	public Client(ChatServer server, ServerSocket serverSocket) throws IOException{
+		this.server = server;
+		clientSocket = serverSocket.accept();
 		outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
 		inputStream = new ObjectInputStream(clientSocket.getInputStream());
 	}
 	
+	public Socket getSocket(){
+		return clientSocket;
+	}
+	
+	public ObjectInputStream getInputStream(){
+		return inputStream;
+	}
+	
+	public ObjectOutputStream getOutputStream(){
+		return outputStream;
+	}
+	
+	public User getConnectUser(){
+		return connectUser;
+	}
+	
+	public void listenToConnection(){
+		Thread clientListener = new Thread(){
+			boolean running = true;
+			public void run() {
+				while(running){
+					try {
+						Packet packet = (Packet)inputStream.readObject();
+						switch (packet.getPacketType()) {
+						case MESSAGE:
+							server.broadcast(packet);
+							break;
+						case CLIENT_ENTER:
+							connectUser = packet.userEnter;
+							server.sendClientLists();
+							break;
+						case CLIENT_EXIT:
+							terminate();
+							running = false;
+							server.broadcast(Packet.sendExit(packet.source));
+							break;
+						case FILE_REQUEST:
+							break;
+						default:
+							break;
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		clientListener.start();
+	}
+	
+	public void terminate(){
+		server.dropClient(this);
+		try {
+			inputStream.close();
+			outputStream.close();
+			clientSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
